725f4aa5a298576e0d09a160ca8ce9bc3fcfc6b2
Aditi-kherkar <aditiits@gmail.com>
2026-05-06 10:06:11 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
 import streamlit as st 
-s.title("Hello i am working worh streamlit library")
+st.title("Hello i am working worh streamlit library")
 st.write("This is my first streamlit application")
